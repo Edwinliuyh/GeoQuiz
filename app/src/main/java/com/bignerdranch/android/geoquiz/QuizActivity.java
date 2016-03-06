f3b5f71accf784492ddfbb2e52bbb36a44576d35
Edwin <liuyh062@qq.com>
2016-03-06 17:45:26 +0800

    修复屏幕旋转后题目回到第一题的问题
    复写onSaveInstanceState(Bundle savedInstanceState)方法

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,8 +1,10 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,8 @@ public class QuizActivity extends Activity {
     ImageButton mNextButton;
     ImageButton mPrevButton;
     TextView mQuestionTextView;
+    private static final String TAG="QuizActivity";
+    private static final String KEY_INDEX="index";
 
     TrueFalse[] mAnswerKey = new TrueFalse[] {
             new TrueFalse(R.string.question_oceans, true),
@@ -31,7 +35,13 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+//      从Bundle中恢复mCurrentIndex
+        if (savedInstanceState!=null){
+            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         updatQuestion();
@@ -78,6 +88,43 @@ public class QuizActivity extends Activity {
                 updatQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG,"onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
     }
 
     private void prevIndex() {
